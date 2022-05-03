14c1a9e9412d1047c53ad6b4b21973b72676848b
Shuruhatik <72819916+Shuruhatik@users.noreply.github.com>
2022-05-03 19:22:32 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,18 +1,23 @@
-const { Client } = require('discord.js');
-const Interaction = require("./Interaction.js")
-module.exports = (client)=>{
-  client.TextInputs = {
-    open:(interaction,textinput)=>{
-      return client.api.interactions(interaction.id)[interaction.token].callback.post({ data: { type: 9, data: textinput.toJSON() } });
+const Interaction = require("./ModalSubmitInteraction.js")
+const DiscordModal = (client)=>{
+  client.modal = {
+    open:(interaction,modaldata)=>{
+      if(modaldata instanceof require("./ModalBuilder.js") && modaldata.toJSON().components && modaldata.toJSON().custom_id  && modaldata.toJSON().title || typeof modaldata == 'object' && modaldata.custom_id && modaldata.components && modaldata.title){
+        return client.api.interactions(interaction.id)[interaction.token].callback.post({ data: { type: 9, data: modaldata instanceof require("./ModalBuilder.js") ? modaldata.toJSON() : modaldata } });
+      } else {
+        throw new Error('INVALID_MODAL');
+      }
     }
   }
   client.ws.on('INTERACTION_CREATE', (data) => {
     if (!data.type) return;
     if(data.type && data.type == 5){
-      client.emit('interactionTextInput', new Interaction(client, data));
+      client.emit('modalSubmitInteraction', new Interaction(client, data));
     }
   });
 } 
-
-module.exports.TextInputField = require("./TextInputField.js")
-module.exports.TextInput = require("./TextInput.js")
+module.exports = DiscordModal
+module.exports.DiscordModal = DiscordModal
+module.exports.ModalField = require("./ModalField.js")
+module.exports.ModalBuilder = require("./ModalBuilder.js")
+module.exports.ModalSubmitFieldsResolver = require("./ModalSubmitFieldsResolver")
